c7ceb1cdda6b936e7a3db36ee2544763e8342ede
giusencito <66499088+giusencito@users.noreply.github.com>
2022-11-06 21:05:02 -0500

    angular material agregado y implementacion de tabla

--- graduates/src/app/app.module.ts
+++ graduates/src/app/app.module.ts
@@ -1,9 +1,12 @@
+import { MaterialModule } from './material/material.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,9 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- graduates/src/app/pages/main-page/main-page.component.ts
+++ graduates/src/app/pages/main-page/main-page.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['year', 'sex', 'type_course', 'no_graduates'];
+  dataSource!:MatTableDataSource<any>;
+  constructor() { 
+    this.dataSource = new MatTableDataSource<any>();
+  }
 
   ngOnInit() {
   }
